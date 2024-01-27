900cbaff0eb26758be8b9c52d51b202e71858566
addania <mirka.schw@gmail.com>
2024-01-27 10:41:14 +0100

    Fetching coordinates from Google API

---
+++ README.md
@@ -0,0 +1,4 @@
+// TO START THE PROJECT:
+npm start
+
+// IT WILL RUN LOCALLY ON: http://localhost:8080/
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,50 @@
-// Code goes here!
+import axios from "axios";
+
+const form = document.querySelector("form")! as HTMLFormElement;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyDcPAiz3cOtGCJRjKeaKE42i9jbK3tX73M";
+
+type GoogleGeoCodingResponse = {
+  results: Array<{ geometry: { location: { lan: number; lng: number } } }>;
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+    enteredAddress
+  )}&key=${GOOGLE_API_KEY}`;
+
+  /* How we can use globally available fetch function (without use of 3rd party library)
+  
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => console.log("my data", data.results[0].geometry.location));
+  */
+  console.log("url", url);
+
+  axios
+    .get<GoogleGeoCodingResponse>(url)
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Ouch");
+      }
+      console.log("response", response);
+      const coordinates = response.data.results[0].geometry.location;
+      console.log("coordinates", coordinates);
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log(error);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
